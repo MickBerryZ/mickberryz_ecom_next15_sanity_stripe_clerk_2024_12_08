21ed9a8fe7b85e60933df7adc2e902fc000b8cc8
mickberryz <sa.thawatchai@gmail.com>
2025-01-04 00:47:51 +0000

    implement live Features

--- app/(store)/page.tsx
+++ app/(store)/page.tsx
@@ -1,9 +1,12 @@
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
+  const products = await getAllProduct();
   return (
     <div className="">
       <h1>Hello World</h1>
+
+      {/* render all the products */}
       <Button>CLick me</Button>
     </div>
   );
